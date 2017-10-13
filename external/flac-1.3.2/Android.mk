f23f2831b039ef9e7d9daeefde4f3aa68c6b31b5
Sam Lantinga <slouken@libsdl.org>
2017-10-12 21:27:20 -0700

    Updated Android native Makefiles for new external libraries

---
+++ external/flac-1.3.2/Android.mk
@@ -0,0 +1,51 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libFLAC
+
+OGG_LIBRARY_PATH := $(LOCAL_PATH)/../libogg-1.3.2
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include \
+                    $(LOCAL_PATH)/src/libFLAC/include \
+                    $(OGG_LIBRARY_PATH)/include \
+                    $(OGG_LIBRARY_PATH)/android
+LOCAL_CFLAGS := -include $(LOCAL_PATH)/android/config.h
+
+LOCAL_SRC_FILES := \
+    src/libFLAC/bitmath.c \
+    src/libFLAC/bitreader.c \
+    src/libFLAC/bitwriter.c \
+    src/libFLAC/cpu.c \
+    src/libFLAC/crc.c \
+    src/libFLAC/fixed.c \
+    src/libFLAC/fixed_intrin_sse2.c \
+    src/libFLAC/fixed_intrin_ssse3.c \
+    src/libFLAC/float.c \
+    src/libFLAC/format.c \
+    src/libFLAC/lpc.c \
+    src/libFLAC/lpc_intrin_sse.c \
+    src/libFLAC/lpc_intrin_sse2.c \
+    src/libFLAC/lpc_intrin_sse41.c \
+    src/libFLAC/lpc_intrin_avx2.c \
+    src/libFLAC/md5.c \
+    src/libFLAC/memory.c \
+    src/libFLAC/metadata_iterators.c \
+    src/libFLAC/metadata_object.c \
+    src/libFLAC/stream_decoder.c \
+    src/libFLAC/stream_encoder.c \
+    src/libFLAC/stream_encoder_intrin_sse2.c \
+    src/libFLAC/stream_encoder_intrin_ssse3.c \
+    src/libFLAC/stream_encoder_intrin_avx2.c \
+    src/libFLAC/stream_encoder_framing.c \
+    src/libFLAC/window.c \
+	src/libFLAC/ogg_decoder_aspect.c \
+	src/libFLAC/ogg_encoder_aspect.c \
+	src/libFLAC/ogg_helper.c \
+	src/libFLAC/ogg_mapping.c \
+    $(OGG_LIBRARY_PATH)/src/framing.c \
+    $(OGG_LIBRARY_PATH)/src/bitwise.c
+
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_C_INCLUDES)
+
+include $(BUILD_SHARED_LIBRARY)
