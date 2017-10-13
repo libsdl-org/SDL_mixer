c981e8fded40f07d4591588363c7a6eb8f34c06f
Sam Lantinga <slouken@libsdl.org>
2017-10-12 21:38:45 -0700

    Updated to 2.0.2

--- SDL_mixer.h
+++ SDL_mixer.h
@@ -40,7 +40,7 @@ extern "C" {
 */
 #define SDL_MIXER_MAJOR_VERSION 2
 #define SDL_MIXER_MINOR_VERSION 0
-#define SDL_MIXER_PATCHLEVEL    1
+#define SDL_MIXER_PATCHLEVEL    2
 
 /* This macro can be used to fill a version structure with the compile-time
  * version of the SDL_mixer library.
