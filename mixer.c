1e8f0db7f260c73e281729c6b9872570a5251750
Ozkan Sezer <sezeroz@gmail.com>
2018-10-07 00:21:02 +0300

    backport fix for bugs 1914, 1915. (from 2.0 branch commit 030181ff9f59).

--- mixer.c
+++ mixer.c
@@ -126,7 +126,7 @@ const char *Mix_GetChunkDecoder(int index)
 
 static void add_chunk_decoder(const char *decoder)
 {
-	void *ptr = SDL_realloc(chunk_decoders, (num_decoders + 1) * sizeof (const char **));
+	void *ptr = SDL_realloc(chunk_decoders, (num_decoders + 1) * sizeof (const char *));
 	if (ptr == NULL) {
 		return;  /* oh well, go on without it. */
 	}
