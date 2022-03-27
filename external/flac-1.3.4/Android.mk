9425f0e422e1b2dea3d8e9a43f69c2209ba8e2bc
Ozkan Sezer <sezeroz@gmail.com>
2022-03-28 01:56:56 +0300

    external libs, flac: Android.mk fixing...

--- external/flac-1.3.4/Android.mk
+++ external/flac-1.3.4/Android.mk
@@ -6,7 +6,10 @@ LOCAL_MODULE := libFLAC
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include \
                     $(LOCAL_PATH)/src/libFLAC/include
-
+#FLAC_OGG_LIBRARY_PATH := ../libogg-1.3.2
+#LOCAL_C_INCLUDES+= $(LOCAL_PATH)/$(FLAC_OGG_LIBRARY_PATH)/include \
+#                   $(LOCAL_PATH)/$(FLAC_OGG_LIBRARY_PATH)/android
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_C_INCLUDES)
 LOCAL_CFLAGS := -include $(LOCAL_PATH)/android/config.h
 
 LOCAL_SRC_FILES := \
@@ -36,12 +39,10 @@ LOCAL_SRC_FILES := \
     src/libFLAC/stream_encoder_intrin_ssse3.c \
     src/libFLAC/stream_encoder_intrin_avx2.c  \
     src/libFLAC/stream_encoder_framing.c \
-    src/libFLAC/window.c \
-    src/libFLAC/ogg_decoder_aspect.c \
-    src/libFLAC/ogg_encoder_aspect.c \
-    src/libFLAC/ogg_helper.c \
-    src/libFLAC/ogg_mapping.c
-
-LOCAL_EXPORT_C_INCLUDES += $(LOCAL_C_INCLUDES)
+    src/libFLAC/window.c
+#   src/libFLAC/ogg_decoder_aspect.c
+#   src/libFLAC/ogg_encoder_aspect.c
+#   src/libFLAC/ogg_helper.c
+#   src/libFLAC/ogg_mapping.c
 
 include $(BUILD_STATIC_LIBRARY)
