2538f76cd3ea9049ac0642e6226c1b2959320754
Simon McVittie <smcv@collabora.com>
2022-05-05 17:57:47 +0100

    autogen.sh: Delete temporary files after running autoconf
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- autogen.sh
+++ autogen.sh
@@ -7,6 +7,8 @@ echo "This may take a while ..."
 
 cat acinclude/* >aclocal.m4
 "${AUTOCONF:-autoconf}"
+rm aclocal.m4
+rm -rf autom4te.cache
 
 # Run configure for this platform
 echo "Now you are ready to run ./configure"
