47afbd1468d824256ba408da5813da231e4d138d
Ozkan Sezer <sezeroz@gmail.com>
2022-07-01 20:55:00 +0300

    SDL_mixer.h: minor corrections to Mix_LoadWAV_RW() 'since' field.

--- include/SDL_mixer.h
+++ include/SDL_mixer.h
@@ -541,7 +541,7 @@ extern DECLSPEC int SDLCALL Mix_AllocateChannels(int numchans);
  * whether this function succeeds or not. SDL_mixer reads everything it needs
  * from the RWops during this call in any case.
  *
- * There is a separate function (macro, before SDL_mixer 2.0.53) to read files
+ * There is a separate function (a macro, before SDL_mixer 2.6.0) to read files
  * from disk without having to deal with SDL_RWops:
  * `Mix_LoadWAV("filename.wav")` will call this function and manage those
  * details for you.
@@ -554,7 +554,8 @@ extern DECLSPEC int SDLCALL Mix_AllocateChannels(int numchans);
  *                to leave it open.
  * \returns a new chunk, or NULL on error.
  *
- * \since This function is available since SDL_mixer 2.0.0.
+ * \since This function is available since SDL_mixer 2.6.0 (and as a macro
+ *        since 2.0.0).
  *
  * \sa Mix_LoadWAV
  * \sa Mix_FreeChunk
