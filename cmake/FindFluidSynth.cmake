a1a12981b4a1d4d1d8b627c17c3e8095b32392bf
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-03-07 18:26:41 -0500

    CMake: Use pkg-config in find modules.
    This allows capturing more information such as transitive dependencies.
    These hints are used as default values for the existing cache
    variables, so users can still edit/overwrite them if they wish so.

--- cmake/FindFluidSynth.cmake
+++ cmake/FindFluidSynth.cmake
@@ -1,18 +1,29 @@
 include(FindPackageHandleStandardArgs)
 
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_FLUIDSYNTH QUIET fluidsynth)
+
 find_library(FluidSynth_LIBRARY
     NAMES fluidsynth
+    HINTS ${PC_FLUIDSYNTH_LIBDIR}
 )
 
 find_path(FluidSynth_INCLUDE_PATH
     NAMES fluidsynth.h
+    HINTS ${PC_FLUIDSYNTH_INCLUDEDIR}
 )
 
-set(FluidSynth_COMPILE_OPTIONS "" CACHE STRING "Extra compile options of FluidSynth")
+if(PC_FLUIDSYNTH_FOUND)
+    get_flags_from_pkg_config("${FluidSynth_LIBRARY}" "PC_FLUIDSYNTH" "_fluidsynth")
+endif()
 
-set(FluidSynth_LINK_LIBRARIES "" CACHE STRING "Extra link libraries of FluidSynth")
+set(FluidSynth_COMPILE_OPTIONS "${_fluidsynth_compile_options}" CACHE STRING "Extra compile options of FluidSynth")
 
-set(FluidSynth_LINK_OPTIONS "" CACHE STRING "Extra link flags of FluidSynth")
+set(FluidSynth_LINK_LIBRARIES "${_fluidsynth_link_libraries}" CACHE STRING "Extra link libraries of FluidSynth")
+
+set(FluidSynth_LINK_OPTIONS "${_fluidsynth_link_options}" CACHE STRING "Extra link flags of FluidSynth")
+
+set(FluidSynth_LINK_DIRECTORIES "${_fluidsynth_link_directories}" CACHE PATH "Extra link directories of FluidSynth")
 
 find_package_handle_standard_args(FluidSynth
     REQUIRED_VARS FluidSynth_LIBRARY FluidSynth_INCLUDE_PATH
@@ -27,6 +38,7 @@ if(FluidSynth_FOUND)
             INTERFACE_COMPILE_OPTIONS "${FluidSynth_COMPILE_OPTIONS}"
             INTERFACE_LINK_LIBRARIES "${FluidSynth_LINK_LIBRARIES}"
             INTERFACE_LINK_OPTIONS "${FluidSynth_LINK_OPTIONS}"
+            INTERFACE_LINK_DIRECTORIES "${FluidSynth_LINK_DIRECTORIES}"
         )
     endif()
 endif()
--- cmake/Findgme.cmake
+++ cmake/Findgme.cmake
@@ -1,18 +1,29 @@
 include(FindPackageHandleStandardArgs)
 
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_GME QUIET libgme)
+
 find_library(gme_LIBRARY
     NAMES gme
+    HINTS ${PC_GME_LIBDIR}
 )
-
-set(gme_COMPILE_OPTIONS "" CACHE STRING "Extra compile options of gme")
-
-set(gme_LINK_LIBRARIES "" CACHE STRING "Extra link libraries of gme")
-
-set(gme_LINK_OPTIONS "" CACHE STRING "Extra link flags of gme")
 
 find_path(gme_INCLUDE_PATH
     NAMES gme/gme.h
+    HINTS ${PC_GME_INCLUDEDIR}
 )
+
+if(PC_GME_FOUND)
+    get_flags_from_pkg_config("${gme_LIBRARY}" "PC_GME" "_gme")
+endif()
+
+set(gme_COMPILE_OPTIONS "${_gme_compile_options}" CACHE STRING "Extra compile options of gme")
+
+set(gme_LINK_LIBRARIES "${_gme_link_libraries}" CACHE STRING "Extra link libraries of gme")
+
+set(gme_LINK_OPTIONS "${_gme_link_options}" CACHE STRING "Extra link flags of gme")
+
+set(gme_LINK_DIRECTORIES "${_gme_link_directories}" CACHE PATH "Extra link directories of gme")
 
 find_package_handle_standard_args(gme
     REQUIRED_VARS gme_LIBRARY gme_INCLUDE_PATH
@@ -31,6 +42,7 @@ if(gme_FOUND)
             INTERFACE_COMPILE_OPTIONS "${gme_COMPILE_OPTIONS}"
             INTERFACE_LINK_LIBRARIES "${gme_LINK_LIBRARIES}"
             INTERFACE_LINK_OPTIONS "${gme_LINK_OPTIONS}"
+            INTERFACE_LINK_DIRECTORIES "${gme_LINK_DIRECTORIES}"
         )
     endif()
 endif()
