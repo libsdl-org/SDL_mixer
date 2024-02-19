libsdl-org/SDL_mixer

commit 6c4e80c09d820c0dabb082e0bd14cf447dc05f16

Changed 'freesrc' parameter from int to SDL_bool

src/codecs/load_voc.c
@@ -371,28 +371,43 @@ static Uint32 voc_read(SDL_RWops *src, vs_t *v, Uint8 *buf, SDL_AudioSpec *spec)
 
 
 /* don't call this directly; use Mix_LoadWAV_RW() for now. */
-SDL_AudioSpec *Mix_LoadVOC_RW (SDL_RWops *src, int freesrc,
+SDL_AudioSpec *Mix_LoadVOC_RW (SDL_RWops *src, SDL_bool freesrc,
         SDL_AudioSpec *spec, Uint8 **audio_buf, Uint32 *audio_len)
 {
+    SDL_bool was_error = SDL_TRUE;
     vs_t v;
-    int was_error = 1;
     int samplesize;
     Uint8 *fillptr;
     void *ptr;
 
-    if ((!src) || (!audio_buf) || (!audio_len))   /* sanity checks. */
+    /* Sanity checks */
+    if (audio_buf) {
+        *audio_buf = NULL;
+    }
+    if (!src) {
+        SDL_InvalidParamError("src");
         goto done;
+    }
+    if (!spec) {
+        SDL_InvalidParamError("spec");
+        goto done;
+    }
+    if (!audio_buf) {
+        SDL_InvalidParamError("audio_buf");
+        goto done;
+    }
+    if (!audio_len) {
+        SDL_InvalidParamError("audio_len");
+        goto done;
+    }
 
     if (!voc_check_header(src))
         goto done;
 
-    SDL_memset(&v, 0, sizeof(vs_t));
+    SDL_zero(v);
     v.rate = VOC_BAD_RATE;
     v.rest = 0;
     v.has_extended = 0;
-    *audio_buf = NULL;
-    *audio_len = 0;
-    SDL_memset(spec, '\0', sizeof(SDL_AudioSpec));
 
     if (!voc_get_block(src, &v, spec))
         goto done;
@@ -407,6 +422,7 @@ SDL_AudioSpec *Mix_LoadVOC_RW (SDL_RWops *src, int freesrc,
         goto done;
     }
 
+    SDL_zerop(spec);
     spec->format = ((v.size == ST_SIZE_WORD) ? SDL_AUDIO_S16 : SDL_AUDIO_U8);
     if (spec->channels == 0)
         spec->channels = v.channels;
@@ -427,9 +443,6 @@ SDL_AudioSpec *Mix_LoadVOC_RW (SDL_RWops *src, int freesrc,
         ptr = SDL_realloc(*audio_buf, *audio_len);
         if (ptr == NULL)
         {
-            SDL_free(*audio_buf);
-            *audio_buf = NULL;
-            *audio_len = 0;
             goto done;
         }
 
@@ -439,22 +452,28 @@ SDL_AudioSpec *Mix_LoadVOC_RW (SDL_RWops *src, int freesrc,
 
     spec->samples = (Uint16)(*audio_len / v.size);
 
-    was_error = 0;  /* success, baby! */
-
     /* Don't return a buffer that isn't a multiple of samplesize */
     samplesize = ((spec->format & 0xFF)/8)*spec->channels;
     *audio_len &= (Uint32) ~(samplesize-1);
+
+    was_error = SDL_FALSE;
 
 done:
     if (freesrc && src) {
         SDL_RWclose(src);
     }
-
     if (was_error) {
+        if (audio_buf && *audio_buf) {
+            SDL_free(*audio_buf);
+            *audio_buf = NULL;
+        }
+        if (audio_len) {
+            *audio_len = 0;
+        }
         spec = NULL;
     }
+    return spec;
 
-    return(spec);
 } /* Mix_LoadVOC_RW */
 
 /* end of load_voc.c ... */

include/SDL3/SDL_mixer.h
@@ -553,7 +553,7 @@ extern DECLSPEC int SDLCALL Mix_AllocateChannels(int numchans);
  * fly. Also, crucially, there are as many channels for chunks as the app can
  * allocate, but SDL_mixer only offers a single "music" channel.
  *
- * If `freesrc` is non-zero, the RWops will be closed before returning,
+ * If `freesrc` is SDL_TRUE, the RWops will be closed before returning,
  * whether this function succeeds or not. SDL_mixer reads everything it needs
  * from the RWops during this call in any case.
  *
@@ -566,7 +566,7 @@ extern DECLSPEC int SDLCALL Mix_AllocateChannels(int numchans);
  * Mix_FreeChunk().
  *
  * \param src an SDL_RWops that data will be read from.
- * \param freesrc non-zero to close/free the SDL_RWops before returning, zero
+ * \param freesrc SDL_TRUE to close/free the SDL_RWops before returning, SDL_FALSE
  *                to leave it open.
  * \returns a new chunk, or NULL on error.
  *
@@ -576,7 +576,7 @@ extern DECLSPEC int SDLCALL Mix_AllocateChannels(int numchans);
  * \sa Mix_LoadWAV
  * \sa Mix_FreeChunk
  */
-extern DECLSPEC Mix_Chunk * SDLCALL Mix_LoadWAV_RW(SDL_RWops *src, int freesrc);
+extern DECLSPEC Mix_Chunk * SDLCALL Mix_LoadWAV_RW(SDL_RWops *src, SDL_bool freesrc);
 
 /**
  * Load a supported audio format into a chunk.
@@ -603,7 +603,7 @@ extern DECLSPEC Mix_Chunk * SDLCALL Mix_LoadWAV_RW(SDL_RWops *src, int freesrc);
  * Mix_FreeChunk().
  *
  * Note that before SDL_mixer 2.6.0, this function was a macro that called
- * Mix_LoadWAV_RW(), creating a RWops and setting `freesrc` to 1. This macro
+ * Mix_LoadWAV_RW(), creating a RWops and setting `freesrc` to SDL_TRUE. This macro
  * has since been promoted to a proper API function. Older binaries linked
  * against a newer SDL_mixer will still call Mix_LoadWAV_RW directly, as they
  * are using the macro, which was available since the dawn of time.
@@ -668,7 +668,7 @@ extern DECLSPEC Mix_Music * SDLCALL Mix_LoadMUS(const char *file);
  * fly. Also, crucially, there are as many channels for chunks as the app can
  * allocate, but SDL_mixer only offers a single "music" channel.
  *
- * If `freesrc` is non-zero, the RWops will be closed before returning,
+ * If `freesrc` is SDL_TRUE, the RWops will be closed before returning,
  * whether this function succeeds or not. SDL_mixer reads everything it needs
  * from the RWops during this call in any case.
  *
@@ -684,7 +684,7 @@ extern DECLSPEC Mix_Music * SDLCALL Mix_LoadMUS(const char *file);
  * Mix_FreeMusic().
  *
  * \param src an SDL_RWops that data will be read from.
- * \param freesrc non-zero to close/free the SDL_RWops before returning, zero
+ * \param freesrc SDL_TRUE to close/free the SDL_RWops before returning, SDL_FALSE
  *                to leave it open.
  * \returns a new music object, or NULL on error.
  *
@@ -692,7 +692,7 @@ extern DECLSPEC Mix_Music * SDLCALL Mix_LoadMUS(const char *file);
  *
  * \sa Mix_FreeMusic
  */
-extern DECLSPEC Mix_Music * SDLCALL Mix_LoadMUS_RW(SDL_RWops *src, int freesrc);
+extern DECLSPEC Mix_Music * SDLCALL Mix_LoadMUS_RW(SDL_RWops *src, SDL_bool freesrc);
 
 /**
  * Load an audio format into a music object, assuming a specific format.
@@ -728,7 +728,7 @@ extern DECLSPEC Mix_Music * SDLCALL Mix_LoadMUS_RW(SDL_RWops *src, int freesrc);
  * - `MUS_OPUS` (Opus files)
  * - `MUS_WAVPACK` (WavPack files)
  *
- * If `freesrc` is non-zero, the RWops will be closed before returning,
+ * If `freesrc` is SDL_TRUE, the RWops will be closed before returning,
  * whether this function succeeds or not. SDL_mixer reads everything it needs
  * from the RWops during this call in any case.
  *
@@ -741,7 +741,7 @@ extern DECLSPEC Mix_Music * SDLCALL Mix_LoadMUS_RW(SDL_RWops *src, int freesrc);
  *
  * \param src an SDL_RWops that data will be read from.
  * \param type the type of audio data provided by `src`.
- * \param freesrc non-zero to close/free the SDL_RWops before returning, zero
+ * \param freesrc SDL_TRUE to close/free the SDL_RWops before returning, SDL_FALSE
  *                to leave it open.
  * \returns a new music object, or NULL on error.
  *
@@ -749,7 +749,7 @@ extern DECLSPEC Mix_Music * SDLCALL Mix_LoadMUS_RW(SDL_RWops *src, int freesrc);
  *
  * \sa Mix_FreeMusic
  */
-extern DECLSPEC Mix_Music * SDLCALL Mix_LoadMUSType_RW(SDL_RWops *src, Mix_MusicType type, int freesrc);
+extern DECLSPEC Mix_Music * SDLCALL Mix_LoadMUSType_RW(SDL_RWops *src, Mix_MusicType type, SDL_bool freesrc);
 
 /**
  * Load a WAV file from memory as quickly as possible.

src/codecs/load_aiff.c
@@ -62,10 +62,10 @@ static Uint32 SANE_to_Uint32 (Uint8 *sanebuf)
 
 /* This function is based on SDL_LoadWAV_RW(). */
 
-SDL_AudioSpec *Mix_LoadAIFF_RW (SDL_RWops *src, int freesrc,
+SDL_AudioSpec *Mix_LoadAIFF_RW (SDL_RWops *src, SDL_bool freesrc,
     SDL_AudioSpec *spec, Uint8 **audio_buf, Uint32 *audio_len)
 {
-    int was_error;
+    SDL_bool was_error = SDL_TRUE;
     int found_SSND;
     int found_COMM;
     int found_VHDR;
@@ -91,10 +91,24 @@ SDL_AudioSpec *Mix_LoadAIFF_RW (SDL_RWops *src, int freesrc,
     Uint8 sane_freq[10];
     Uint32 frequency = 0;
 
-    /* Make sure we are passed a valid data source */
-    was_error = 0;
-    if (src == NULL) {
-        was_error = 1;
+    /* Sanity checks */
+    if (audio_buf) {
+        *audio_buf = NULL;
+    }
+    if (!src) {
+        SDL_InvalidParamError("src");
+        goto done;
+    }
+    if (!spec) {
+        SDL_InvalidParamError("spec");
+        goto done;
+    }
+    if (!audio_buf) {
+        SDL_InvalidParamError("audio_buf");
+        goto done;
+    }
+    if (!audio_len) {
+        SDL_InvalidParamError("audio_len");
         goto done;
     }
 
@@ -109,7 +123,6 @@ SDL_AudioSpec *Mix_LoadAIFF_RW (SDL_RWops *src, int freesrc,
     }
     if ((FORMchunk != FORM) || ((AIFFmagic != AIFF) && (AIFFmagic != _8SVX))) {
         Mix_SetError("Unrecognized file type (not AIFF nor 8SVX)");
-        was_error = 1;
         goto done;
     }
 
@@ -144,13 +157,11 @@ SDL_AudioSpec *Mix_LoadAIFF_RW (SDL_RWops *src, int freesrc,
                 samplesize  = SDL_ReadBE16(src);
                 if (SDL_RWread(src, sane_freq, sizeof(sane_freq)) != sizeof(sane_freq)) {
                     Mix_SetError("Bad AIFF sample frequency");
-                    was_error = 1;
                     goto done;
                 }
                 frequency   = SANE_to_Uint32(sane_freq);
                 if (frequency == 0) {
                     Mix_SetError("Bad AIFF sample frequency");
-                    was_error = 1;
                     goto done;
                 }
                 break;
@@ -183,30 +194,26 @@ SDL_AudioSpec *Mix_LoadAIFF_RW (SDL_RWops *src, int freesrc,
 
     if ((AIFFmagic == AIFF) && !found_SSND) {
         Mix_SetError("Bad AIFF (no SSND chunk)");
-        was_error = 1;
         goto done;
     }
 
     if ((AIFFmagic == AIFF) && !found_COMM) {
         Mix_SetError("Bad AIFF (no COMM chunk)");
-        was_error = 1;
         goto done;
     }
 
     if ((AIFFmagic == _8SVX) && !found_VHDR) {
         Mix_SetError("Bad 8SVX (no VHDR chunk)");
-        was_error = 1;
         goto done;
     }
 
     if ((AIFFmagic == _8SVX) && !found_BODY) {
         Mix_SetError("Bad 8SVX (no BODY chunk)");
-        was_error = 1;
         goto done;
     }
 
     /* Decode the audio data format */
-    SDL_memset(spec, 0, sizeof(*spec));
+    SDL_zerop(spec);
     spec->freq = frequency;
     switch (samplesize) {
         case 8:
@@ -217,7 +224,6 @@ SDL_AudioSpec *Mix_LoadAIFF_RW (SDL_RWops *src, int freesrc,
             break;
         default:
             Mix_SetError("Unsupported AIFF samplesize");
-            was_error = 1;
             goto done;
     }
     spec->channels = (Uint8) channels;
@@ -227,25 +233,34 @@ SDL_AudioSpec *Mix_LoadAIFF_RW (SDL_RWops *src, int freesrc,
     *audio_buf = (Uint8 *)SDL_malloc(*audio_len);
     if (*audio_buf == NULL) {
         Mix_OutOfMemory();
-        return(NULL);
+        goto done;
     }
     SDL_RWseek(src, start, SDL_RW_SEEK_SET);
     if (SDL_RWread(src, *audio_buf, *audio_len) != *audio_len) {
         Mix_SetError("Unable to read audio data");
-        return(NULL);
+        goto done;
     }
 
     /* Don't return a buffer that isn't a multiple of samplesize */
     *audio_len &= ~((samplesize / 8) - 1);
+
+    was_error = SDL_FALSE;
 
 done:
     if (freesrc && src) {
         SDL_RWclose(src);
     }
     if (was_error) {
+        if (audio_buf && *audio_buf) {
+            SDL_free(*audio_buf);
+            *audio_buf = NULL;
+        }
+        if (audio_len) {
+            *audio_len = 0;
+        }
         spec = NULL;
     }
-    return(spec);
+    return spec;
 }
 
 /* vi: set ts=4 sw=4 expandtab: */

src/codecs/load_aiff.h
@@ -27,7 +27,7 @@
 */
 
 /* Don't call this directly; use Mix_LoadWAV_RW() for now. */
-SDL_AudioSpec *Mix_LoadAIFF_RW (SDL_RWops *src, int freesrc,
+SDL_AudioSpec *Mix_LoadAIFF_RW (SDL_RWops *src, SDL_bool freesrc,
     SDL_AudioSpec *spec, Uint8 **audio_buf, Uint32 *audio_len);
 
 /* vi: set ts=4 sw=4 expandtab: */

src/codecs/load_sndfile.c
@@ -117,9 +117,10 @@ static sf_count_t sfvio_tell(void *user_data)
     return SDL_RWtell(RWops);
 }
 
-SDL_AudioSpec *Mix_LoadSndFile_RW (SDL_RWops *src, int freesrc,
+SDL_AudioSpec *Mix_LoadSndFile_RW (SDL_RWops *src, SDL_bool freesrc,
         SDL_AudioSpec *spec, Uint8 **audio_buf, Uint32 *audio_len)
 {
+    SDL_bool was_error = SDL_TRUE;
     SNDFILE *sndfile = NULL;
     SF_INFO sfinfo;
     SF_VIRTUAL_IO sfvio = {
@@ -132,16 +133,26 @@ SDL_AudioSpec *Mix_LoadSndFile_RW (SDL_RWops *src, int freesrc,
     Uint32 len;
     short *buf = NULL;
 
-    int was_error = 1;
-
-    if (src == NULL || spec == NULL ||
-        audio_buf == NULL || audio_len == NULL) {
+    /* Sanity checks */
+    if (audio_buf) {
+        *audio_buf = NULL;
+    }
+    if (!src) {
+        SDL_InvalidParamError("src");
         goto done;
     }
-
-    *audio_buf = NULL;
-    *audio_len = 0;
-    SDL_memset(spec, 0, sizeof(*spec));
+    if (!spec) {
+        SDL_InvalidParamError("spec");
+        goto done;
+    }
+    if (!audio_buf) {
+        SDL_InvalidParamError("audio_buf");
+        goto done;
+    }
+    if (!audio_len) {
+        SDL_InvalidParamError("audio_len");
+        goto done;
+    }
 
     if (SNDFILE_loaded == 0) {
         if (SNDFILE_init() != 0) {
@@ -182,8 +193,7 @@ SDL_AudioSpec *Mix_LoadSndFile_RW (SDL_RWops *src, int freesrc,
         goto done;
     }
 
-    was_error = 0;
-
+    SDL_zerop(spec);
     spec->channels = sfinfo.channels;
     spec->freq = sfinfo.samplerate;
     spec->format = SDL_AUDIO_S16;
@@ -191,25 +201,31 @@ SDL_AudioSpec *Mix_LoadSndFile_RW (SDL_RWops *src, int freesrc,
     *audio_buf = (Uint8 *)buf;
     *audio_len = len;
 
-    if (freesrc && src) {
-        SDL_RWclose(src);
-    }
+    was_error = SDL_FALSE;
 
 done:
     if (sndfile) {
         SF_sf_close(sndfile);
     }
-
+    if (freesrc && src) {
+        SDL_RWclose(src);
+    }
     if (was_error) {
+        if (audio_buf && *audio_buf) {
+            SDL_free(*audio_buf);
+            *audio_buf = NULL;
+        }
+        if (audio_len) {
+            *audio_len = 0;
+        }
         spec = NULL;
     }
-
     return spec;
 }
 
 #else
 
-SDL_AudioSpec *Mix_LoadSndFile_RW (SDL_RWops *src, int freesrc,
+SDL_AudioSpec *Mix_LoadSndFile_RW (SDL_RWops *src, SDL_bool freesrc,
         SDL_AudioSpec *spec, Uint8 **audio_buf, Uint32 *audio_len)
 {
     (void) src;

src/codecs/load_sndfile.h
@@ -31,7 +31,7 @@
 #include <SDL3/SDL_mixer.h>
 
 /* Don't call this directly; use Mix_LoadWAV_RW() for now. */
-SDL_AudioSpec *Mix_LoadSndFile_RW (SDL_RWops *src, int freesrc,
+SDL_AudioSpec *Mix_LoadSndFile_RW (SDL_RWops *src, SDL_bool freesrc,
         SDL_AudioSpec *spec, Uint8 **audio_buf, Uint32 *audio_len);
 
 void SNDFILE_uninit (void);

src/codecs/load_voc.h
@@ -30,7 +30,7 @@
 */
 
 /* Don't call this directly; use Mix_LoadWAV_RW() for now. */
-SDL_AudioSpec *Mix_LoadVOC_RW (SDL_RWops *src, int freesrc,
+SDL_AudioSpec *Mix_LoadVOC_RW (SDL_RWops *src, SDL_bool freesrc,
         SDL_AudioSpec *spec, Uint8 **audio_buf, Uint32 *audio_len);
 
 /* vi: set ts=4 sw=4 expandtab: */

src/codecs/music_drflac.c
@@ -51,7 +51,7 @@ typedef struct {
     struct mp3file_t file;
     drflac *dec;
     int play_count;
-    int freesrc;
+    SDL_bool freesrc;
     int volume;
     int status;
     int sample_rate;
@@ -158,7 +158,7 @@ static void DRFLAC_MetaCB(void *context, drflac_metadata *metadata)
 
 static int DRFLAC_Seek(void *context, double position);
 
-static void *DRFLAC_CreateFromRW(SDL_RWops *src, int freesrc)
+static void *DRFLAC_CreateFromRW(SDL_RWops *src, SDL_bool freesrc)
 {
     DRFLAC_Music *music;
 

commit 6aebc4eec0a79c7fe7a780939a4ac303b27712cb

load_voc.c: fix spec losing retrieved frequency
(Was somehow broken by commit 6c4e80c09d820c0dabb082e0bd14cf447dc05f16)
Fixes https://github.com/libsdl-org/SDL_mixer/issues/593

src/codecs/load_voc.c
@@ -413,6 +413,7 @@ SDL_AudioSpec *Mix_LoadVOC_RW (SDL_RWops *src, SDL_bool freesrc,
     v.rate = VOC_BAD_RATE;
     v.rest = 0;
     v.has_extended = 0;
+    SDL_zerop(spec);
 
     if (!voc_get_block(src, &v, spec)) {
         goto done;
@@ -428,7 +429,6 @@ SDL_AudioSpec *Mix_LoadVOC_RW (SDL_RWops *src, SDL_bool freesrc,
         goto done;
     }
 
-    SDL_zerop(spec);
     spec->format = ((v.size == ST_SIZE_WORD) ? SDL_AUDIO_S16 : SDL_AUDIO_U8);
     if (spec->channels == 0) {
         spec->channels = v.channels;