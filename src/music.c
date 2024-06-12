25d4c3eca2e3b2cbab0ccde2b96e513149b9707a
Ryan C. Gordon <icculus@icculus.org>
2024-06-12 19:01:20 -0400

    Updated for latest wikiheaders.
    (cherry picked from commit 7baa66f30930a21d8d24583fcc390dd354407f80)

--- src/music.c
+++ src/music.c
@@ -1544,7 +1544,7 @@ const char* Mix_GetSoundFonts(void)
     return NULL;
 }
 
-int Mix_EachSoundFont(int (SDLCALL *function)(const char*, void*), void *data)
+int Mix_EachSoundFont(Mix_EachSoundFontCallback function, void *data)
 {
     char *context, *path, *paths;
     const char* cpaths = Mix_GetSoundFonts();
