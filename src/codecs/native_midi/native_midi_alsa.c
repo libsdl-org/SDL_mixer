78409577639cb0a81df77134fc6ce15a17f10e45
Simon Howard <fraggle@soulsphere.org>
2024-09-17 18:47:29 -0400

    native_midi: Open ALSA device, start thread
    We don't write any MIDI data to the sequencer yet but this sets up the
    basic foundations. For now, the playback thread just exits immediately,
    so `playmus` completes successfully.

--- src/codecs/native_midi/native_midi_alsa.c
+++ src/codecs/native_midi/native_midi_alsa.c
@@ -21,6 +21,8 @@
 #include "SDL_config.h"
 #ifdef __LINUX__
 
+#include <alsa/asoundlib.h>
+
 #include "native_midi.h"
 #include "native_midi_common.h"
 
@@ -29,11 +31,37 @@ struct _NativeMidiSong {
     MIDIEvent *event_list;
 };
 
-static NativeMidiSong *currentsong;
+static enum { STOPPED, PLAYING, SHUTDOWN } state = STOPPED;
+static SDL_Thread *native_midi_thread;
+static snd_seq_t *output;
+static int output_queue;
 
 int native_midi_detect(void)
 {
-    return 0;
+    int err;
+
+    if (output != NULL) {
+        return 1;
+    }
+
+    // TODO: Allow output port to be specified explicitly
+    err = snd_seq_open(&output, "default", SND_SEQ_OPEN_OUTPUT, 0);
+    if (err < 0) {
+        SDL_Log("native_midi_detect: Failed to open sequencer device: %s",
+                snd_strerror(err));
+        return 0;
+    }
+    snd_seq_set_client_name(output, "SDL_mixer");
+    snd_seq_set_output_buffer_size(output, 512);
+
+    output_queue = snd_seq_alloc_queue(output);
+    if (output_queue < 0) {
+        snd_seq_close(output);
+        output = NULL;
+        return 0;
+    }
+
+    return 1;
 }
 
 NativeMidiSong *native_midi_loadsong_RW(SDL_RWops *src, int freesrc)
@@ -58,25 +86,55 @@ void native_midi_freesong(NativeMidiSong *song)
     SDL_free(song);
 }
 
+static int playback_thread(void *data)
+{
+    NativeMidiSong *song = data;
+
+    snd_seq_start_queue(output, output_queue, NULL);
+
+    while (state == PLAYING) {
+        // TODO
+        break;
+    }
+
+    state = STOPPED;
+    return 0;
+}
+
 void native_midi_start(NativeMidiSong *song, int loops)
 {
+    native_midi_stop();
+    state = PLAYING;
+    native_midi_thread = SDL_CreateThread(
+        playback_thread, "native midi playback", song);
 }
 
 void native_midi_pause(void)
 {
+    snd_seq_stop_queue(output, output_queue, NULL);
 }
 
 void native_midi_resume(void)
 {
+    snd_seq_continue_queue(output, output_queue, NULL);
 }
 
 void native_midi_stop(void)
 {
+    if (state != PLAYING) {
+        return;
+    }
+
+    state = SHUTDOWN;
+    SDL_WaitThread(native_midi_thread, NULL);
+
+    snd_seq_drop_output(output);
+    snd_seq_stop_queue(output, output_queue, NULL);
 }
 
 int native_midi_active(void)
 {
-    return 0;
+    return state == PLAYING;
 }
 
 void native_midi_setvolume(int volume)
