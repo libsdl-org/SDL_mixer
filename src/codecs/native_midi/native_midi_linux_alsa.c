1b1d7bacf8b6b28e7d9403f9ce482a49c7db2183
Ozkan Sezer <sezeroz@gmail.com>
2025-03-28 14:20:50 +0300

    native_midi_linux_alsa.c: define _POSIX_C_SOURCE and _GNU_SOURCE
    .. if not already defined
    
    Fixes https://github.com/libsdl-org/SDL_mixer/issues/668

--- src/codecs/native_midi/native_midi_linux_alsa.c
+++ src/codecs/native_midi/native_midi_linux_alsa.c
@@ -22,6 +22,14 @@
 #include <SDL3/SDL_platform.h>
 #ifdef SDL_PLATFORM_LINUX
 
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#endif
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
+
 #include <SDL3/SDL_hints.h>
 #include <SDL3/SDL_atomic.h>
 
