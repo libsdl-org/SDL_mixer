sezero/libtimidity

readsbk.c: change READSTR() macro into a procedure. (from timidity++).

src/readsbk.c
@@ -70,7 +70,18 @@ static int READW(uint16 *vp, FILE *fd)
 	return 1;
 }
 
-#define READSTR(var,fd)	fread(var, 20, 1, fd)
+static int READSTR(char *str, FILE *fd)
+{
+	int n;
+	if (fread(str, 20, 1, fd) != 1) return -1;
+	str[19] = '\0';
+	n = strlen(str);
+	while (n > 0 && str[n - 1] == ' ')
+		n--;
+	str[n] = '\0';
+	return n;
+}
+
 #define READID(var,fd)	fread(var, 1, 4, fd)
 #define READB(var,fd)	fread(var, 1, 1, fd)
 #define SKIPB(fd)	{uint8 dummy; fread(&dummy, 1, 1, fd);}

libsdl-org/SDL_mixer

src/timidity/readsbk.c
@@ -44,7 +44,18 @@ static int READW(Uint16 *vp, SDL_IOStream *io)
 	return 1;
 }
 
-#define READSTR(var,io)	SDL_ReadIO(io, var, 20)
+static int READSTR(char *str, SDL_IOStream *io)
+{
+	int n;
+	if (SDL_ReadIO(io, str, 20) != 20) return -1;
+	str[19] = '\0';
+	n = (int) SDL_strlen(str);
+	while (n > 0 && str[n - 1] == ' ')
+		n--;
+	str[n] = '\0';
+	return n;
+}
+
 #define READID(var,io)	SDL_ReadIO(io, var, 4)
 #define READB(var,io)	SDL_ReadIO(io, var, 1)
 #define SKIPB(io)	SDL_SeekIO(io, 1, SDL_IO_SEEK_CUR);