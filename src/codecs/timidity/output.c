ab58ca41153d786eea550d73a8a7776ead2017f9
Ozkan Sezer <sezeroz@gmail.com>
2026-08-20 23:40:40 +0300

    timidity: fix S32 and F32 output:
    timidity internal output isn't full-width 32 bits:
    GUARD_BITS should be taken into account.
    
    Reference issue: https://github.com/icculus/SDL_sound/issues/128 .
    
    (cherry picked from commit f9bf1c9d67d7e7cf0c5f257be3a30b2908cc1ccc)

--- src/codecs/timidity/output.c
+++ src/codecs/timidity/output.c
@@ -101,7 +101,7 @@ void timi_s32tof32(void *dp, Sint32 *lp, Sint32 c)
   float *sp=(float *)(dp);
   while (c--)
     {
-      *sp++ = (float)(*lp++) / 2147483647.0f;
+      *sp++ = (float)(*lp++) / (float)(1<<(32 - GUARD_BITS - 1));
     }
 }
 
@@ -110,7 +110,7 @@ void timi_s32tof32x(void* dp, Sint32* lp, Sint32 c)
     float* sp = (float*)(dp);
     while (c--)
     {
-        *sp++ = SDL_SwapFloat((float)(*lp++) / 2147483647.0f);
+        *sp++ = SDL_SwapFloat((float)(*lp++) / (float)(1<<(32 - GUARD_BITS - 1)));
     }
 }
 
@@ -119,7 +119,7 @@ void timi_s32tos32(void *dp, Sint32 *lp, Sint32 c)
   Sint32 *sp=(Sint32 *)(dp);
   while (c--)
     {
-      *sp++ = (*lp++);
+      *sp++ = (*lp++)<<GUARD_BITS;
     }
 }
 
@@ -128,6 +128,6 @@ void timi_s32tos32x(void *dp, Sint32 *lp, Sint32 c)
   Sint32 *sp=(Sint32 *)(dp);
   while (c--)
     {
-      *sp++ = SDL_Swap32(*lp++);
+      *sp++ = SDL_Swap32((*lp++)<<GUARD_BITS);
     }
 }
