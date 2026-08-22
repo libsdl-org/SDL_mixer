icculus/SDL_sound

timidity: move load_sbk() stuff from song load to Timidity_Init() time
Timidity_SetSoundfont() now loads the specified file, therefore its
result should be checked before calling Timidity_Init(). If loading
the font fails, the font won't be set in the library.

src/timidity/sndfont.c
@@ -28,8 +28,6 @@
  * compile flags
  *----------------------------------------------------------------*/
 
-/*#define SF_CLOSE_EACH_FILE*/
-
 /*#define SF_SUPPRESS_ENVELOPE*/
 /*#define SF_SUPPRESS_TREMOLO*/
 /*#define SF_SUPPRESS_VIBRATO*/
@@ -125,6 +123,7 @@ static void calc_filterQ(Layer *lay, SFInfo *sf, SampleList *sp);
 
 
 static SFInsts sfrec;
+static SFInfo sfinfo;
 static SFExclude *sfexclude;
 static SFOrder *sforder;
 
@@ -133,11 +132,8 @@ static const int cutoff_allowed = 0;
 #endif
 
 
-int init_soundfont(MidiSong *song, const char *fname, int order)
+int init_sbk(const char *fname)
 {
-	static SFInfo sfinfo;
-	int i;
-
 	SNDDBG(("init soundfonts `%s'\n", fname));
 
 	SDL_memset(&sfinfo, 0, sizeof(sfinfo));
@@ -146,12 +142,37 @@ int init_soundfont(MidiSong *song, const char *fname, int order)
 		SNDDBG(("can't open soundfont file %s\n", fname));
 		return -1;
 	}
+
 	sfrec.fname = SDL_strdup(fname);
-	if (!sfrec.fname) goto nomem;
+	if (!sfrec.fname) goto fail;
+
 	if (load_sbk(sfrec.io, &sfinfo) < 0) {
 		SNDDBG(("%s: bad soundfont file\n", fname));
 		goto fail;
 	}
+
+	return 0;
+
+fail:
+	end_sbk();
+	return -1;
+}
+
+void end_sbk(void)
+{
+	if (sfrec.io) {
+		SDL_CloseIO(sfrec.io);
+		sfrec.io = NULL;
+	}
+	SDL_free(sfrec.fname);
+	sfrec.fname = NULL;
+	free_sbk(&sfinfo);
+	SDL_memset(&sfinfo, 0, sizeof(sfinfo));
+}
+
+int init_soundfont(MidiSong *song, int order)
+{
+	int i;
 
 	for (i = 0; i < sfinfo.nrpresets - 1; i++) {
 		int bank = sfinfo.presethdr[i].bank;
@@ -182,20 +203,9 @@ int init_soundfont(MidiSong *song, const char *fname, int order)
 	sfrec.samplepos = sfinfo.samplepos;
 	sfrec.samplesize = sfinfo.samplesize;
 
-	free_sbk(&sfinfo);
-
-#ifdef SF_CLOSE_EACH_FILE
-	SDL_CloseIO(sfrec.io);
-	sfrec.io = NULL;
-#endif
 	return 0;
 nomem:
 	song->oom = 1;
-fail:	SDL_CloseIO(sfrec.io);
-	sfrec.io = NULL;
-	SDL_free(sfrec.fname);
-	sfrec.fname = NULL;
-	free_sbk(&sfinfo);
 	return -1;
 }
 
@@ -213,13 +223,6 @@ static void free_sample(InstList *ip)
 void end_soundfont(void)
 {
 	InstList *ip, *next;
-
-	if (sfrec.io) {
-		SDL_CloseIO(sfrec.io);
-		sfrec.io = NULL;
-	}
-	SDL_free(sfrec.fname);
-	sfrec.fname = NULL;
 
 	for (ip = sfrec.instlist; ip; ip = next) {
 		next = ip->next;
@@ -242,12 +245,8 @@ Instrument *load_soundfont(MidiSong *song, int order, int bank, int preset, int 
 	Instrument *inst = NULL;
 
 	if (sfrec.io == NULL) {
-		if (sfrec.fname == NULL)
-			return NULL;
-		if ((sfrec.io = timi_openfile(sfrec.fname)) == NULL) {
-			SNDDBG(("can't open soundfont file %s\n", sfrec.fname));
-			return NULL;
-		}
+		SNDDBG(("NULL soundfont file pointer\n"));
+		return NULL;
 	}
 
 	for (ip = sfrec.instlist; ip; ip = ip->next) {
@@ -258,11 +257,6 @@ Instrument *load_soundfont(MidiSong *song, int order, int bank, int preset, int 
 	}
 	if (ip && ip->samples)
 		inst = load_from_file(song, &sfrec, ip);
-
-#ifdef SF_CLOSE_EACH_FILE
-	SDL_CloseIO(sfrec.io);
-	sfrec.io = NULL;
-#endif
 
 	return inst;
 }

libsdl-org/SDL_mixer

src/codecs/timidity/sndfont.c
@@ -27,8 +27,6 @@
  * compile flags
  *----------------------------------------------------------------*/
 
-/*#define SF_CLOSE_EACH_FILE*/
-
 /*#define SF_SUPPRESS_ENVELOPE*/
 /*#define SF_SUPPRESS_TREMOLO*/
 /*#define SF_SUPPRESS_VIBRATO*/
@@ -124,6 +122,7 @@ static void calc_filterQ(Layer *lay, SFInfo *sf, SampleList *sp);
 
 
 static SFInsts sfrec;
+static SFInfo sfinfo;
 static SFExclude *sfexclude;
 static SFOrder *sforder;
 
@@ -132,11 +131,8 @@ static const int cutoff_allowed = 0;
 #endif
 
 
-int init_soundfont(MidiSong *song, const char *fname, int order)
+int init_sbk(const char *fname)
 {
-	static SFInfo sfinfo;
-	int i;
-
 	SNDDBG(("init soundfonts `%s'\n", fname));
 
 	SDL_memset(&sfinfo, 0, sizeof(sfinfo));
@@ -145,12 +141,37 @@ int init_soundfont(MidiSong *song, const char *fname, int order)
 		SNDDBG(("can't open soundfont file %s\n", fname));
 		return -1;
 	}
+
 	sfrec.fname = SDL_strdup(fname);
-	if (!sfrec.fname) goto nomem;
+	if (!sfrec.fname) goto fail;
+
 	if (load_sbk(sfrec.rw, &sfinfo) < 0) {
 		SNDDBG(("%s: bad soundfont file\n", fname));
 		goto fail;
 	}
+
+	return 0;
+
+fail:
+	end_sbk();
+	return -1;
+}
+
+void end_sbk(void)
+{
+	if (sfrec.rw) {
+		SDL_RWclose(sfrec.rw);
+		sfrec.rw = NULL;
+	}
+	SDL_free(sfrec.fname);
+	sfrec.fname = NULL;
+	free_sbk(&sfinfo);
+	SDL_memset(&sfinfo, 0, sizeof(sfinfo));
+}
+
+int init_soundfont(MidiSong *song, int order)
+{
+	int i;
 
 	for (i = 0; i < sfinfo.nrpresets - 1; i++) {
 		int bank = sfinfo.presethdr[i].bank;
@@ -181,20 +202,9 @@ int init_soundfont(MidiSong *song, const char *fname, int order)
 	sfrec.samplepos = sfinfo.samplepos;
 	sfrec.samplesize = sfinfo.samplesize;
 
-	free_sbk(&sfinfo);
-
-#ifdef SF_CLOSE_EACH_FILE
-	SDL_RWclose(sfrec.rw);
-	sfrec.rw = NULL;
-#endif
 	return 0;
 nomem:
 	song->oom = 1;
-fail:	SDL_RWclose(sfrec.rw);
-	sfrec.rw = NULL;
-	SDL_free(sfrec.fname);
-	sfrec.fname = NULL;
-	free_sbk(&sfinfo);
 	return -1;
 }
 
@@ -212,13 +222,6 @@ static void free_sample(InstList *ip)
 void end_soundfont(void)
 {
 	InstList *ip, *next;
-
-	if (sfrec.rw) {
-		SDL_RWclose(sfrec.rw);
-		sfrec.rw = NULL;
-	}
-	SDL_free(sfrec.fname);
-	sfrec.fname = NULL;
 
 	for (ip = sfrec.instlist; ip; ip = next) {
 		next = ip->next;
@@ -241,12 +244,8 @@ Instrument *load_soundfont(MidiSong *song, int order, int bank, int preset, int 
 	Instrument *inst = NULL;
 
 	if (sfrec.rw == NULL) {
-		if (sfrec.fname == NULL)
-			return NULL;
-		if ((sfrec.rw = timi_openfile(sfrec.fname)) == NULL) {
-			SNDDBG(("can't open soundfont file %s\n", sfrec.fname));
-			return NULL;
-		}
+		SNDDBG(("NULL soundfont file pointer\n"));
+		return NULL;
 	}
 
 	for (ip = sfrec.instlist; ip; ip = ip->next) {
@@ -257,11 +256,6 @@ Instrument *load_soundfont(MidiSong *song, int order, int bank, int preset, int 
 	}
 	if (ip && ip->samples)
 		inst = load_from_file(song, &sfrec, ip);
-
-#ifdef SF_CLOSE_EACH_FILE
-	SDL_RWclose(sfrec.rw);
-	sfrec.rw = NULL;
-#endif
 
 	return inst;
 }